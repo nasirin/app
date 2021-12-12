b63cb1ff5869b5e45319adc6824c71c740e07ed3
nasirin50 <34644046+nasirin@users.noreply.github.com>
2021-12-12 20:46:58 +0700

    refactoring user profile page

--- frontend/console/resources/views/partials/header.blade.php
+++ frontend/console/resources/views/partials/header.blade.php
@@ -36,7 +36,7 @@
                      </li>
 
                      <li>
-                         <a class="dropdown-item d-flex align-items-center" href="/template/users-profile.html">
+                         <a class="dropdown-item d-flex align-items-center" href="/profile">
                              <i class="bi bi-person"></i>
                              <span>My Profile</span>
                          </a>
--- frontend/console/routes/web.php
+++ frontend/console/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/profile', [UserController::class, 'index']);
